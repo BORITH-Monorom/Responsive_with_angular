a8ed1da566120cf4c33d9f021415f8d5fd67511c
BORITH-Monorom <borithmonorom@gmail.com>
2023-09-20 10:59:59 +0700

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener, } from '@angular/core';
 @Component({
   selector: 'app-root',
+
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
-  title = 'my-app';
+  slidesPerView:number = 3;
+  screenWidth!: number;
+
+  @HostListener('window:resize')
+  getScreenWidth(){
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth >= 320 && this.screenWidth <= 480){
+      this.slidesPerView = 1;
+    }
+   else if(this.screenWidth >= 480 && this.screenWidth <= 992){
+      this.slidesPerView = 2;
+    }
+    else if(this.screenWidth >= 992 && this.screenWidth <= 1200){
+      this.slidesPerView = 3;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {MatTabsModule} from '@angular/material/tabs'; 
 import { AppComponent } from './app.component';
-
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// register Swiper custom elements
+register();
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
